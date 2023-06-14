1158fed9fe90fd535b1aa1f4defe9ff7b412378f
MichaelB94 <124793101+MichaelB94@users.noreply.github.com>
2023-06-14 17:06:55 +0200

    articlesService

--- src/main/java/com/example/EasyBlog/Repositories/ArticlesRepository.java
+++ src/main/java/com/example/EasyBlog/Repositories/ArticlesRepository.java
@@ -2,14 +2,25 @@ package com.example.EasyBlog.Repositories;
 
 import com.example.EasyBlog.Entity.Articles;
 import com.example.EasyBlog.Entity.Enum.TypeStatusEnum;
+import com.example.EasyBlog.Entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ArticlesRepository extends JpaRepository<Articles,Long> {
+
+    @Query("SELECT a FROM articles a WHERE a.title = ?1")
+    Optional<Articles> findArticlesByTitle (String title);
     @Query("SELECT a FROM articles a WHERE a.typeStatus='ACTIVE'")
-    Optional<Articles>findByActive(TypeStatusEnum typeStatus);
+    Optional<List<Articles>>findByActive();
+
+    @Query("SELECT a FROM articles a WHERE a.typeStatus='INACTIVE'")
+    Optional<List<Articles>>findByInactive();
+
+    @Query("SELECT a FROM articles a WHERE a.typeStatus='SUSPENDED'")
+    Optional<List<Articles>>findBySuspended();
 
     // TODO filtrare solo quelli attivi
 }
--- src/main/java/com/example/EasyBlog/Service/ArticlesService.java
+++ src/main/java/com/example/EasyBlog/Service/ArticlesService.java
@@ -7,7 +7,6 @@ import com.example.EasyBlog.Repositories.ArticlesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,47 +15,67 @@ public class ArticlesService {
     @Autowired
     private static ArticlesRepository articlesRepository;
 
+    public Articles createArticles(Articles articles) {
+        Optional<Articles> articleByTitle = articlesRepository.findArticlesByTitle(articles.getTitle());
+        if (articleByTitle.isPresent()){
+            throw new IllegalStateException("Title already exists");
+        }
+        return articlesRepository.save(articles);
+    }
+
     public Articles getArticlesById(Long id) throws Exception {
         Optional<Articles> articlesOptional = articlesRepository.findById(id);
         if (articlesOptional.isEmpty()) {
             throw new IllegalArgumentException("Article does not exist");
         }
         Articles article = articlesOptional.get();
-        if (article.getTypeStatus() != TypeStatusEnum.ACTIVE) {
-            throw new IllegalArgumentException("Article is not in an active status");
+        if (article.getTypeStatus() == TypeStatusEnum.INACTIVE) {
+            throw new IllegalArgumentException("Article is inactive");
+        } else if (article.getTypeStatus() == TypeStatusEnum.SUSPENDED) {
+            throw new IllegalArgumentException("Article is suspended");
+        } else if (article.getTypeStatus() == TypeStatusEnum.ACTIVE) {
+            return article;
+        } else {
+            throw new IllegalArgumentException("Article is in an unrecognized status");
         }
-        return article;
     }
 
-    public List<Articles> getAllArticles(){
-        List<Articles> activeArticles = new ArrayList<>();
-        List<Articles> allArticles = articlesRepository.findAll();
-        for (Articles articles : allArticles){
-            if (articles.getTypeStatus() == TypeStatusEnum.ACTIVE){
-                activeArticles.add(articles);
-            }
-        }
+    public Optional<List<Articles>> getAllArticles(){
+        Optional<List<Articles>> activeArticles = articlesRepository.findByActive();
         return activeArticles;
     }
-    public List<Articles> getAllDeletedArticles(){
-        List<Articles> inactiveArticles = new ArrayList<>();
-        List<Articles> allArticles = articlesRepository.findAll();
-        for (Articles articles : allArticles){
-            if (articles.getTypeStatus() == TypeStatusEnum.INACTIVE){
-                inactiveArticles.add(articles);
-            }
-        }
+
+    public Optional<List<Articles>> getAllInactiveArticles(){
+        Optional<List<Articles>> inactiveArticles = articlesRepository.findByInactive();
         return inactiveArticles;
     }
 
-    public List<Articles> getAllSuspendedArticles(){
-        List<Articles> suspendedArticles = new ArrayList<>();
-        List<Articles> allArticles = articlesRepository.findAll();
-        for (Articles articles : allArticles){
-            if (articles.getTypeStatus() == TypeStatusEnum.INACTIVE){
-                suspendedArticles.add(articles);
-            }
-        }
+    public Optional<List<Articles>> getAllSuspendedArticles(){
+        Optional<List<Articles>> suspendedArticles = articlesRepository.findBySuspended();
         return suspendedArticles;
     }
+
+
+
+//    public List<Articles> getAllDeletedArticles(){
+//        List<Articles> inactiveArticles = new ArrayList<>();
+//        List<Articles> allArticles = articlesRepository.findAll();
+//        for (Articles articles : allArticles){
+//            if (articles.getTypeStatus() == TypeStatusEnum.INACTIVE){
+//                inactiveArticles.add(articles);
+//            }
+//        }
+//        return inactiveArticles;
+//    }
+//
+//    public List<Articles> getAllSuspendedArticles(){
+//        List<Articles> suspendedArticles = new ArrayList<>();
+//        List<Articles> allArticles = articlesRepository.findAll();
+//        for (Articles articles : allArticles){
+//            if (articles.getTypeStatus() == TypeStatusEnum.INACTIVE){
+//                suspendedArticles.add(articles);
+//            }
+//        }
+//        return suspendedArticles;
+//    }
 }
