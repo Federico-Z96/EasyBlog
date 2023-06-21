178dac8d138d6f43c11e29a138fb26b505dcbd73
Mario Tolvo <mariobros9@hotmail.it>
2023-06-21 17:30:37 +0200

    Pulizia

--- src/main/java/com/example/EasyBlog/Repositories/ArticlesRepository.java
+++ src/main/java/com/example/EasyBlog/Repositories/ArticlesRepository.java
@@ -10,24 +10,20 @@ import java.util.Optional;
 
 public interface ArticlesRepository extends JpaRepository<Articles,Long> {
 
-    @Query("UPDATE Articles SET typeStatus = ? WHERE id = ? ")
+    @Query("UPDATE Articles SET type_status = ? WHERE id = ? ")
     Optional<Articles> updateArticlesStatus(Long articleId);
 
-    @Query("SELECT a FROM Articles a WHERE a.typeGender = ?1")
+    @Query("SELECT a FROM Articles a WHERE a.type_gender = ?1")
     Optional<List<Articles>> getArticlesByGender(TypeGenderArticlesEnum typeGenderArticlesEnum);
 
     @Query("SELECT a FROM Articles a WHERE a.title = ?1")
     Optional<Articles> findArticlesByTitle (String title);
-    @Query("SELECT a FROM Articles a WHERE a.typeStatus='ACTIVE'")
+    @Query("SELECT a FROM Articles a WHERE a.type_status='ACTIVE'")
     Optional<List<Articles>>findArticlesByActive();
 
-    @Query("SELECT a FROM Articles a WHERE a.typeStatus='INACTIVE'")
+    @Query("SELECT a FROM Articles a WHERE a.type_status='INACTIVE'")
     Optional<List<Articles>>findArticlesByInactive();
 
-    @Query("SELECT a FROM Articles a WHERE a.typeStatus='SUSPENDED'")
+    @Query("SELECT a FROM Articles a WHERE a.type_status='SUSPENDED'")
     Optional<List<Articles>>findArticlesBySuspended();
-
-
-
-    // TODO filtrare solo quelli attivi
 }
--- src/main/java/com/example/EasyBlog/Repositories/UsersRepository.java
+++ src/main/java/com/example/EasyBlog/Repositories/UsersRepository.java
@@ -22,13 +22,13 @@ public interface UsersRepository extends JpaRepository<Users,Long> {
     @Query("SELECT u FROM users u WHERE u.email = ?1")
     Optional<Users> findUserByEmail (String Email);
 
-    @Query("SELECT u FROM users u WHERE u.typeStatus='ACTIVE'")
+    @Query("SELECT u FROM users u WHERE u.type_status='ACTIVE'")
     Optional<List<Users>> findUsersByActive();
 
-    @Query("SELECT u FROM users u WHERE u.typeStatus='INACTIVE'")
+    @Query("SELECT u FROM users u WHERE u.type_status='INACTIVE'")
     Optional<List<Users>>findUsersByInactive();
 
-    @Query("SELECT u FROM Users u WHERE u.typeStatus='SUSPENDED'")
+    @Query("SELECT u FROM Users u WHERE u.type_status='SUSPENDED'")
     Optional<List<Users>>findUsersBySuspended();
 
 }
